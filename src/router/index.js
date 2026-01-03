f2e79e02ea0671060362eb9b8386468e812450e2
faqihasyari <faqih8158@gmail.com>
2026-01-03 19:19:04 +0700

    add router index

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,63 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { useAuthStore } from '@/stores/auth.store'
+
+// import views
+import LoginView from '@/views/LoginView.vue'
+import DashboardView from '@/views/DashboardView.vue'
+import AttendanceView from '@/views/AttendanceView.vue'
+import PermissionViw from '@/views/PermissionView.vue'
+const routes = [
+  {
+    path: '/login',
+    name: 'Login',
+    component: LoginView,
+    meta: { guest: true },
+  },
+  {
+    path: '/',
+    redirect: '/dashboard',
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardView,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: '/permission',
+    name: 'Permission',
+    component: PermissionViw,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: '/attendance',
+    name: 'Attendance',
+    component: AttendanceView,
+    meta: { requiresAuth: true },
+  },
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
+  history: createWebHistory(),
+  routes,
+})
+
+// pelindung route
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore()
+  const isLoggedIn = authStore.isLoggedIn
+
+  // guard butuh login
+  if (to.meta.requiresAuth && !isLoggedIn) {
+    return next({ name: 'Login' })
+  }
+
+  // guard guest
+  if (to.meta.guest && isLoggedIn) {
+    return next({ name: 'Dashboard' })
+  }
+
+  next()
 })
 
 export default router
